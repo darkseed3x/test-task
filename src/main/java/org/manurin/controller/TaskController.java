fbd2cc516fa42473ee181cbaa4c6a0bad6f403ef
Manurin Vasily (EXT) <vasily.manurin@iiii-tech.com>
2023-03-30 14:19:14 +0300

    init

---
+++ src/main/java/org/manurin/controller/TaskController.java
@@ -0,0 +1,21 @@
+package org.manurin.controller;
+
+import org.manurin.api.TariffApi;
+import org.manurin.model.BundledProduct;
+import org.manurin.model.Tariff;
+
+import java.util.List;
+
+public class TaskController implements TariffApi {
+
+
+    @Override
+    public void tariffAdd(Tariff tariff) {
+
+    }
+
+    @Override
+    public List<BundledProduct> tariffGet() {
+        return null;
+    }
+}
